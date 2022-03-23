3d2fcce2d2af8930cabe498621efafdadd35c625
Jacob Deery <jacob.deery@gmail.com>
2022-03-23 00:20:12 -0400

    Initial commit

---
+++ mask_rcnn/train_net.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# Copyright (c) Facebook, Inc. and its affiliates.
+"""
+A main training script.
+
+This scripts reads a given config file and runs the training or evaluation.
+It is an entry point that is made to train standard models in detectron2.
+
+In order to let one script support training of many models,
+this script contains logic that are specific to these built-in models and therefore
+may not be suitable for your own project.
+For example, your research project perhaps only needs a single "evaluator".
+
+Therefore, we recommend you to use detectron2 as an library and take
+this file as an example of how to use the library.
+You may want to write your own script with your datasets and other customizations.
+"""
+
+import imp
+import logging
+import os
+from collections import OrderedDict
+
+import detectron2.utils.comm as comm
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, hooks, launch
+from detectron2.modeling import GeneralizedRCNNWithTTA
+
+from cityscapes_custom_evaluator import CityscapesPixelwiseInstanceEvaluator
+
+
+class Trainer(DefaultTrainer):
+    """
+    We use the "DefaultTrainer" which contains pre-defined default logic for
+    standard training workflow. They may not work for you, especially if you
+    are working on a new research project. In that case you can write your
+    own training loop. You can use "tools/plain_train_net.py" as an example.
+    """
+
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        return CityscapesPixelwiseInstanceEvaluator(dataset_name)
+
+
+def setup(args):
+    """
+    Create configs and perform basic setups.
+    """
+    cfg = get_cfg()
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+    default_setup(cfg, args)
+    return cfg
+
+
+def main(args):
+    cfg = setup(args)
+
+    if args.eval_only:
+        model = Trainer.build_model(cfg)
+        DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
+            cfg.MODEL.WEIGHTS, resume=args.resume
+        )
+        res = Trainer.test(cfg, model)
+        return res
+
+    """
+    If you'd like to do anything fancier than the standard training logic,
+    consider writing your own training loop (see plain_train_net.py) or
+    subclassing the trainer.
+    """
+    trainer = Trainer(cfg)
+    trainer.resume_or_load(resume=args.resume)
+
+    return trainer.train()
+
+
+if __name__ == "__main__":
+    args = default_argument_parser().parse_args()
+    print("Command Line Args:", args)
+    launch(
+        main,
+        args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        args=(args,),
+    )
---
+++ mask_rcnn/uncertainty.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def get_uncertainty(masks):
+    h, w = masks[0].shape
+
+    total_masks = len(masks)
+
+    centroids = np.full((h, w, total_masks, 2), np.nan)
+
+    x_unc = np.zeros((h, w, 1))
+    y_unc = np.zeros((h, w, 1))
+
+    x_coords = np.arange(w)
+    y_coords = np.arange(h)
+    xx, yy = np.meshgrid(x_coords, y_coords)
+
+    for i, mask in enumerate(masks):
+        array_mask = np.logical_not(mask)
+        x_cent = np.mean(np.ma.masked_array(xx, mask=array_mask))
+        y_cent = np.mean(np.ma.masked_array(yy, mask=array_mask))
+        centroids[:, :, i, :][mask == 1] = (x_cent, y_cent)
+
+    stds = np.nanstd(centroids, axis=2)
+    x_unc = stds[:, :, 0]
+    y_unc = stds[:, :, 1]
+
+    return np.nan_to_num(x_unc + y_unc)
